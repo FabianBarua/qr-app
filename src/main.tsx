01fb16a4d08e7451240608a11cefe0feb664595b
fabianbarua <baruafabian@gmail.com>
2024-09-16 13:56:21 -0400

    Actualizar enrutamiento de la página principal

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Route, Switch, useParams } from "wouter";
+import { Route, Switch } from "wouter";
 import App from './App';
 import { Providers } from './components/Provider';
-
-const links = [{
-  id: "1",
-  url: "https://www.google.com"
-}, {
-  id: "2",
-  url: "https://www.facebook.com"
-}, {
-  id: "3",
-  url: "https://www.twitter.com"
-}];
-
-
-// eslint-disable-next-line react-refresh/only-export-components
-const ToRedirect = () => {
-
-  const params = useParams();
-  const { id } = params;
-
-  if (!params || !id) return null;
-
-  const urlSelected = links.find(link => link.id === id)?.url;
-
-  window.location.href = urlSelected || "https://www.google.com";
-
-}
 
 createRoot(document.getElementById('root')!).render(
   <Providers>
     <Switch>
-      <Route path="/:id" component={ToRedirect} />
+      <Route path="/:id" component={App} />
       <Route>
         <App />
       </Route>
